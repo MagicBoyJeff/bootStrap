313deafa108433cadb5c6ef815e67e789fbcd5bc
hujie <13065193297@163.com>
2019-09-24 19:49:15 +0800

    bootStrap first commit

---
+++ src/main/java/正则表达式/RegestTest.java
@@ -0,0 +1,16 @@
+package 正则表达式;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegestTest {
+    public static void main(String[] args) {
+        String str ="成都市(武侯区)(成化区)(高校区)";
+        Pattern p = Pattern.compile(".*?(?=\\()");
+        Matcher matcher = p.matcher( str );
+        if(matcher.find()){
+            System.out.println(matcher.group(0));
+
+        }
+    }
+}
